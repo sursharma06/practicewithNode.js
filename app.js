b519be93cc930ebf50db25246df05f9b4491afeb
Surbhi Sharma <sharma.surbhi13@gmail.com>
2017-10-03 14:20:37 -0400

    serve a file

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-var http = require('http');
-
-var server = http.createServer(function (req, res) {
-  res.statusCode = 404;
-  res.setHeader('Content-type', 'text/html');
-  res.end('<h1>Hello World</h1> <p>hows everyone doing?</p>');
-});
-
-server.listen(8000, function () {
-  console.log('server listening on port 8000...');
-});
+// var http = require('http');
+//
+// var server = http.createServer(function (req, res) {
+//   res.statusCode = 404;
+//   res.setHeader('Content-type', 'text/html');
+//   res.end('<h1>Hello World</h1> <p>hows everyone doing?</p>');
+// });
+//
+// server.listen(8000, function () {
+//   console.log('server listening on port 8000...');
+// });
 
 // var url = require('url');
 // console.log(url.parse('http://www.example.com:80/path?query#fragment'));
+
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (req, res) {
+  fs.readFile('index.html', function (err, data) {
+    if (err) {
+      throw err;
+    }
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'index.html');
+    res.end(data);
+  });
+});
+
+server.listen(8000, function () {
+  console.log('hello');
+});
